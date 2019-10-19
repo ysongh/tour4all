e2069e579892adc9efa9ad287e684250d8cb0738
You Song Hou <yousonghou@yahoo.com>
2019-10-19 10:59:27 -0400

    add input

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,27 +2,64 @@ import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
 import Button from '../components/common/Button';
+import Input from '../components/common/Input';
 
 class Login extends Component{
+    state = {
+        leaderSelected: false,
+        userSelected: false,
+        showInput: false
+    };
+
+    inputForLeader(){
+        this.setState({
+            showInput: true,
+            leaderSelected: true,
+            userSelected: false
+        });
+    };
+
+    inputForUser(){
+        this.setState({
+            showInput: false,
+            leaderSelected: false,
+            userSelected: true
+        });
+    };
+    
     render(){
         const { 
             login,
             login__fbButton,
+            login__Label,
+            login__LabelSelected,
             login__btnGroup,
-            login__btn } = styles;
+            login__btn,
+            login__btnSelected,
+            login__input } = styles;
 
         return(
             <View style={login}>
                 <Button
                     buttonStyle={login__fbButton}
-                    value="Continue With FB"/>
+                    textStyle={login__Label}
+                    value="Continue With FB" />
+
+                {this.state.showInput ? <Input 
+                    placeholder={this.state.leaderSelected ? "ID" : ""}
+                    inputStyle={login__input}/> : null }
+
                 <View style={login__btnGroup}>
                     <Button
-                        buttonStyle={login__btn}
-                        value="Leader"/>
+                        buttonStyle={this.state.leaderSelected ? login__btnSelected : login__btn}
+                        value="Leader"
+                        textStyle={this.state.leaderSelected ? login__Label : login__LabelSelected}
+                        onPress={() => this.inputForLeader()} />
                     <Button
-                        buttonStyle={login__btn}
-                        value="User"/>
+                        buttonStyle={this.state.userSelected ? login__btnSelected : login__btn}
+                        value="User"
+                        textStyle={this.state.userSelected ? login__Label : login__LabelSelected}
+                        onPress={() => this.inputForUser()}/>
                 </View>
             </View>
         );
@@ -35,10 +72,22 @@ const styles = StyleSheet.create({
     },
     login__fbButton:{
         alignSelf: "center",
-        backgroundColor: "#82cfe8",
+        backgroundColor: "#000000",
         borderRadius: 5,
         padding: 10,
         marginTop: 10
+    },
+    login__Label:{
+        fontSize: 15,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#ffffff"
+    },
+    login__LabelSelected:{
+        fontSize: 15,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#000000"
     },
     login__btnGroup:{
         flexDirection: "row",
@@ -47,11 +96,29 @@ const styles = StyleSheet.create({
     },
     login__btn:{
         width: 120,
-        backgroundColor: "#82cfe8",
+        backgroundColor: "#f2ebeb",
+        color: "#ffffff",
         borderRadius: 5,
         padding: 10,
         marginTop: 10
     },
+    login__btnSelected:{
+        width: 120,
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        borderRadius: 5,
+        padding: 10,
+        marginTop: 10
+    },
+    login__input:{
+        height: 40,
+        width: '90%',
+        alignSelf: "center",
+        backgroundColor: '#dcdee0',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 15,
+    }
 })
 
 export default Login;
---
+++ src/components/common/Input.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const Input = ({ label, inputStyle, value, onChangeText, placeholder, secureTextEntry, keyboardType }) => {
+    const { inputArea, input__label, input__field } = styles;
+
+    return(
+        <View style={inputArea}>
+            <Text style={input__label}>{label}</Text>
+            <TextInput
+                value={value}
+                placeholder={placeholder}
+                autoCorrect={false}
+                secureTextEntry={secureTextEntry}
+                onChangeText={onChangeText}
+                keyboardType={keyboardType}
+                clearButtonMode="while-editing"
+                style={inputStyle || input__field} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputArea:{
+        padding: 1,
+        marginTop: 10
+    },
+    input__label:{
+        fontSize: 18,
+        paddingLeft: 5
+    },
+    input__field:{
+        height: 40,
+        width: '100%',
+        backgroundColor: '#dcdee0',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 15,
+    }
+});
+
+export default Input;
